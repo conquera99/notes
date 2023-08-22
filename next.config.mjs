436f37a962a0bf327c80e7a40bf4c20acd086c79
Benny <conquera99@gmail.com>
2023-08-22 11:52:48 +0800

    chore: update package and config

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+import nextPwa from 'next-pwa';
+import runtimeCaching from './src/lib/cache.js';
+
+/** @type {import('next-pwa').PWAConfig} */
+const withPWA = nextPwa({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development',
+	register: true,
+	scope: '/',
+	sw: 'notes-worker.js',
+	skipWaiting: false,
+	runtimeCaching: runtimeCaching,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	productionBrowserSourceMaps: process.env.NODE_ENV === 'production',
+	reactStrictMode: false,
+	swcMinify: process.env.NODE_ENV === 'production' ? true : false,
+	compiler: {
+		removeConsole:
+			process.env.NODE_ENV === 'production'
+				? {
+						exclude: ['error'],
+				  }
+				: {},
+	},
+};
+
+export default withPWA(nextConfig);
