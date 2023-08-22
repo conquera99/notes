d5d909b430296467d2fdf5c727e9ca7b22b403e4
Benny <conquera99@gmail.com>
2023-08-22 15:19:00 +0800

    chore: update card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -13,8 +13,10 @@ export default function Card({ id, title, datetime }: CardProps) {
 			href={`/${id || 'create'}`}
 			className="border-gray-600 border-dotted border rounded-lg p-4 col-span-6 lg:col-span-3 xl:col-span-2 cursor-pointer hover:bg-slate-100 transition-all"
 		>
-			<h2>{title || 'No Title'}</h2>
-			<small>{dayjs(datetime).format('DD MMM YYYY HH:mm')}</small>
+			<h2 className="text-lg font-bold">{title || 'No Title'}</h2>
+			<small className="text-sm">
+				{dayjs(datetime).format('DD MMM YYYY HH:mm')}
+			</small>
 		</Link>
 	);
 }
